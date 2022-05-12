9742a67c1c76bbee4e4ee14f9069850da3bbf06b
Jan Kolařík <kolarikjan02@gmail.com>
2022-05-13 01:03:46 +0200

    theme coded

--- js/javascript.js
+++ js/javascript.js
@@ -1,81 +1,56 @@
-function fixHomepageBanner() 
-{
-    if ($(window).width() > 1200) 
-    {
-        let windowWidth = $("body").width();
-        let fromLeft = document.getElementById("owl-homepageBanner-pixelcheck").getBoundingClientRect().left;
-        if ($('#owl-homepageBanner').length > 0) 
-        {
-            $("#owl-homepageBanner").css("width", windowWidth - fromLeft + "px");
-        }
-        else if ($('#subheader--image').length > 0)
-        {
-            $("#subheader--image").css("width", windowWidth - fromLeft + "px");
-        }
-    }
-}
-function fixLeftBanner()
-{
-    if ($(window).width() > 1650) 
-    {
-        let leftHeight = "88px " + $(".homepage-banner .owl-item img").height() + "px";
-        $(".homepage-banner").css("background-size", leftHeight);
-    }
-}
-
 $(document).ready(function () {
     
-    // Enable (init) Fancybox
     $(".fancybox").fancybox({
+
         'hideOnContentClick': true
+
+    });
+    $(".popup-activator").click(function (e) {
+
+        $(this).closest(".popup-custom").children(".popup-content").css("display", "block");
+
     });
 
-    // Fixing homepage banner before being initiated
-    fixHomepageBanner();
+    $(".popup-content--close").click(function (e) { 
 
-    // Homepage banner
-    $('#owl-homepageBanner').owlCarousel({
-        center: true,
-        items:1,
-        loop:true,
-        margin:1,
-        nav:true,
-        dots:true,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true,
-        navText: ["<img src='img/banner-prev.png'>", "<img src='img/banner-next.png'>"]
-    });
-    
-    // Fixing homepage left floating banner after we know banner height
-    fixLeftBanner();
-
-    // kalkulator app
-    $(".kalkulace-input-clickable").click(function (e) {
-        $(".kalkulace-box").removeClass("active");
-        if ($(this).closest(".kalkulace-box").hasClass("active")) 
-        {
-            $(this).closest(".kalkulace-box").removeClass("active");
-            $(this).children("input[type='radio']").removeAttr("checked");
-        }
-        else
-        {
-            $(this).closest(".kalkulace-box").addClass("active");
-            $(this).children("input[type='radio']").attr("checked", "");
-        }
-    });
-
-    // kalkulator - add gate
-    $(".kalkulace-content-add").click(function (e) { 
         e.preventDefault();
-        let row = $(".kalkulace-content-add-content").html();
-        $(this).closest(".kalkulace-content").children(".kalkulace-rows").append(row);
+        
+        $(this).closest(".popup-content").css("display", "none");
+        
     });
 
-});
-$(window).resize(function () { 
-    fixHomepageBanner();
-    setTimeout(function(){ 
-        fixLeftBanner();
-    }, 250);
+    $(".product-btn-info").click(function (e) {
+
+        e.preventDefault();
+
+        $(this).closest(".product").children(".product-extended").css("display", "block");
+        
+        $('.product-extended--carousel').owlCarousel({
+            center: true,
+            items:3,
+            loop:true,
+            margin:20,
+            nav:true,
+            dots:false,
+            navText: ["<img src='img/pagination1.png'>", "<img src='img/pagination2.png'>"],
+            responsive:{
+                0:{
+                    items:2
+                },
+                768:{
+                    items:3
+                },
+            }
+        });
+
+    });
+
+    $(".porduct-extended--close").click(function (e) { 
+
+        e.preventDefault();
+        
+        $(this).closest(".product-extended").css("display", "none");
+        
+    });
+
 });
